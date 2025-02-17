dde2410a957c763b3557ef12dacb0eeee98c103f
Uaine1 <angeloalcantara243@gmail.com>
2025-02-17 13:54:14 +0800

    Make the player able to shoot

--- code/main.py
+++ code/main.py
@@ -14,14 +14,23 @@ class Game():
         pygame.display.set_caption("Vamps")
         self.running = True
         self.clock = pygame.time.Clock()
+        self.load_images()
 
         #Groups
         self.all_sprites = AllSprites()
+        self.bullet_sprites = pygame.sprite.Group()
         self.collision_sprites = pygame.sprite.Group()
 
         self.setup()
 
-        #Sprites
+        #Gun timer
+        self.can_shoot = True
+        self.shoot_time = 0
+        self.shoot_cd = 100
+
+
+    def load_images(self):
+        self.bullet_surf = pygame.image.load(join("images", "gun", "bullet.png")).convert_alpha()
 
 
     def setup(self):
@@ -39,7 +48,24 @@ class Game():
         for obj in map.get_layer_by_name("Entities"):
             if obj.name == "Player":
                 self.player = Player((obj.x, obj.y), self.all_sprites, self.collision_sprites)
+                self.gun = Gun(self.player, self.all_sprites)
 
+    
+    def input(self):
+        if pygame.mouse.get_pressed()[0] and self.can_shoot:
+            pos = self.gun.rect.center + self.gun.player_direction * 60
+            Bullet(self.bullet_surf, pos , self.gun.player_direction, (self.all_sprites, self.bullet_sprites))
+            self.can_shoot = False
+            self.shoot_time = pygame.time.get_ticks()
+
+        self.gun_timer()
+
+
+    def gun_timer(self):
+        if not self.can_shoot:
+            current_time = pygame.time.get_ticks()
+            if current_time - self.shoot_time >= self.shoot_cd:
+                self.can_shoot = True      
 
     def run(self):
         while self.running:
@@ -52,6 +78,8 @@ class Game():
                     self.running = False
 
             #Update the game
+            self.gun_timer()
+            self.input()
             self.all_sprites.update(dt)
 
             #Draw the game
